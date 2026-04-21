9157389a168de76d81db780c70fc7bbb4863acc2
Nithesh-sp <nithesh.244.2025@gmail.com>
2026-04-22 02:09:38 +0530

    Added a endpoint in application so that kubernetes cluster can check app health  And allowed all IP.

--- application/dailypay/settings.py
+++ application/dailypay/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ix_+nq!_$e)96@_hmi*s3=ne8k1medq#of79^pjwu#8rab(($+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
